500aef1c0b163135f5336d91be4c24e9720f6138
enzoeferreira <enzoeferreira@gmail.com>
2023-03-31 12:40:12 -0300

    [ADDED] registro de nomes e matriz relacionamentos

--- Labs/lab04.c
+++ Labs/lab04.c
@@ -1,34 +1,92 @@
 #include <stdio.h>
 #include <string.h>
 
+#define POPULACAOMAX 1000
+#define NOMEMAX 51
+
 int main()
 {
-    char rawIn[110], nome1[51], nome2[51];
-    char populacao[1000];
-    short int relacionamentos[1000][1000];
-    short int nomesAdicionados;
+    char rawIn[110], nome1[NOMEMAX], nome2[NOMEMAX];
+    char *pString, *pNome;
 
-    scanf(" %[^\n]s", rawIn); // Recebe a entrada
-    // printf("'%s'", rawIn);
+    char populacao[POPULACAOMAX][NOMEMAX];
+    int registrados;
+    int relacionamentos[POPULACAOMAX][POPULACAOMAX];
 
-    char *pcEnd, *pcStart;
-    pcEnd = strstr(rawIn, "conhece"); // Seta o ponteiro final
+    int i, j, flag1, flag2, indexNome1, indexNome2;
 
-    // Extração do Nome 1
-    short int i = 0;
-    for(pcStart = rawIn; pcStart < (pcEnd - 1); pcStart++)
+    while(*(pString = gets(rawIn)) != NULL) // Não foi apenas '\n'
     {
-        // Percorre do inicio de rawIn até 2 antes de "conhece" (ultima letra)
-        nome1[i++] = *pcStart;
-    }
-    nome1[i] = '\0'; // Adiciona o '\0' no fim da cadeia
+        /**
+         * Extração dos nomes
+         */
+        pString = strstr(rawIn, "conhece");
+        i = 0;
+        for(pNome = rawIn; pNome < (pString - 1); pNome++)
+        {
+            nome1[i++] = *pNome;
+        }
+        nome1[i] = '\0';
 
-    // Extração do Nome 2
-    i = 0;
-    for(pcStart = pcEnd + 8; *pcStart != '\0'; pcStart++)
-    {
-        // Percorre depois do "conhece" até o fim da cadeia
-        nome2[i++] = *pcStart;
+        i = 0;
+        for(pNome = pString + 8; *pNome != '\0'; pNome++)
+        {
+            nome2[i++] = *pNome;
+        }
+        nome2[i] = '\0';
+
+        printf("Nome 1: '%s'\n", nome1);
+        printf("Nome 2: '%s'\n", nome2);
+
+        /**
+         * Registro de nomes na matriz
+         */
+
+        // Verificação de nome já registrado
+        flag1 = 0;
+        flag2 = 0;
+        for(i = 0; i < registrados; i++)
+        {
+            printf("populacao[%d]: '%s'\n", i, populacao[i]);
+            if((strcmp(populacao[i], nome1)) == 0)
+            {
+                printf("Nome 1 encontrado!\n");
+                flag1 = 1;
+                indexNome1 = i;
+            }
+            if((strcmp(populacao[i], nome2)) == 0)
+            {
+                printf("Nome 2 encontrado!\n");
+                flag2 = 1;
+                indexNome2 = i;
+            }
+        }
+
+        // Registra os nomes não registrados
+        if(!flag1)
+        {
+            indexNome1 = i;
+            strcpy(populacao[i++], nome1);
+            registrados++;
+        }
+        if(!flag1)
+        {
+            indexNome2 = i;
+            strcpy(populacao[i], nome2);
+            registrados++;
+        }
+
+        /**
+         * Manipulação da matriz
+         */
+
+        // Preenche a diagonal principal com 1 (Conhece si mesmo)
+        for(i = 0; i < registrados; i++)
+        {
+            relacionamentos[i][i] = 1;
+        }
+        relacionamentos[indexNome1][indexNome2] = 1; // nome1 conhece nome2
     }
-    nome2[i] = '\0'; // Adiciona o '\0' no fim da cadeia
+
+    
 }
