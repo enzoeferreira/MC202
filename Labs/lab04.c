e0205009375f3766a704ee1f1c01f8f8bbbf4275
enzoeferreira <enzoeferreira@gmail.com>
2023-04-01 17:32:16 -0300

    [MODIFIED] fgets no lugar de gets, removido prints para teste

--- Labs/lab04.c
+++ Labs/lab04.c
@@ -6,7 +6,7 @@
 
 int main()
 {
-    char rawIn[110], nome1[NOMEMAX], nome2[NOMEMAX];
+    char rawIn[111], nome1[NOMEMAX], nome2[NOMEMAX];
     char *pString, *pNome;
 
     char populacao[POPULACAOMAX][NOMEMAX];
@@ -15,7 +15,7 @@ int main()
 
     int i, j, score, flagCeleb, flag1, flag2, indexNome1, indexNome2;
 
-    while(*(pString = gets(rawIn)) != NULL) // Não foi apenas '\n'
+    while(*(pString = fgets(rawIn, 111, stdin)) != '\n') // Não foi apenas '\n'
     {
         /**
          * Extração dos nomes
@@ -29,14 +29,14 @@ int main()
         nome1[i] = '\0';
 
         i = 0;
-        for(pNome = pString + 8; *pNome != '\0'; pNome++)
+        for(pNome = pString + 8; *pNome != '\n'; pNome++)
         {
             nome2[i++] = *pNome;
         }
         nome2[i] = '\0';
 
-        printf("Nome 1: '%s'\n", nome1);
-        printf("Nome 2: '%s'\n", nome2);
+        // printf("Nome 1: '%s'\n", nome1);
+        // printf("Nome 2: '%s'\n", nome2);
 
         /**
          * Registro de nomes no vetor
@@ -47,16 +47,16 @@ int main()
         flag2 = 0;
         for(i = 0; i < registrados; i++)
         {
-            printf("populacao[%d]: '%s'\n", i, populacao[i]);
+            // printf("populacao[%d]: '%s'\n", i, populacao[i]);
             if((strcmp(populacao[i], nome1)) == 0)
             {
-                printf("Nome 1 encontrado!\n");
+                // printf("Nome 1 encontrado!\n");
                 flag1 = 1;
                 indexNome1 = i;
             }
             if((strcmp(populacao[i], nome2)) == 0)
             {
-                printf("Nome 2 encontrado!\n");
+                // printf("Nome 2 encontrado!\n");
                 flag2 = 1;
                 indexNome2 = i;
             }
@@ -93,16 +93,22 @@ int main()
     while(i < registrados)
     {
         if(relacionamentos[i][0] == registrados)
+        {
             // É conhecido por todos
             if(relacionamentos[i][1] == 0)
+            {
                 // Não conhece ninguem
-                printf("%s e' celebridade.", populacao[i]);
+                printf("%s e' celebridade.\n", populacao[i]);
                 if(flagCeleb == 1)
+                {
                     // Celebridade não tinha sido encontrada até agora
                     flagCeleb = 0;
+                }
+            }
+        }
         i++;
     }
     if(flagCeleb)
         // Nenhuma celebridade
-        printf("Nao ha' celebridade.");
+        printf("Nao ha' celebridade.\n");
 }
