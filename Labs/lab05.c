4a0a4b623969ed90acbf3b2950dde371aeca186b
enzoeferreira <enzoeferreira@gmail.com>
2023-04-26 18:08:07 -0300

    [MODIFIED] Alocação dinâmica do vetor

--- Labs/lab05.c
+++ Labs/lab05.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define INMAX 100
 
+typedef struct
+{
+    int linha, coluna, valor, status;
+} tripla;
+
 int main()
 {
-    typedef struct
-    {
-        int linha, coluna, valor, status;
-    } triplet;
     
     int i, m, n, elementos, elementosInvalidos, linha, coluna, valor, encontrado;
     char rawIn[INMAX];
 
     // Criação do vetor que armazenará triplas dos não-nulos de uma matriz m x n
     scanf("%d %d", &m, &n);
-    triplet vetor[m+n];
+    tripla *triplas, *p;
+    triplas = malloc((m+n) * sizeof(tripla));
+    if(triplas == NULL)
+    {
+        return 1;
+    }
+    
+    p = triplas;
 
     // Recebimento das triplas
     scanf("%d", &elementos);
@@ -22,15 +31,17 @@ int main()
     {
         getchar(); // Pega o \n do buffer
         // [a,b] x -> Linha 'a', coluna 'b' tem valor 'x'
-        scanf("[%d,%d] %d", &vetor[i].linha, &vetor[i].coluna, &vetor[i].valor);
-        vetor[i].status = 1;
+        scanf("[%d,%d] %d", &p->linha, &p->coluna, &p->valor);
+        p->status = 1;
+        p++;
     }
     elementosInvalidos = 0;
 
     // Status 0 no restante dos elementos
-    while(i < m + n)
+    while(i < (m + n))
     {
-        vetor[i].status = 0;
+        p->status = 0;
+        p++;
         i++;
     }
 
@@ -41,122 +52,145 @@ int main()
     getchar();
     while(*(fgets(rawIn, INMAX, stdin)) != 't') // Modo 't' finaliza o programa
     {
-        if(*rawIn == 'a')
+        switch(*rawIn)
         {
-            // Modo 'a'
-            sscanf(rawIn, "a [%d,%d] %d", &linha, &coluna, &valor);
-            i = 0;
-            encontrado = 0;
-            while(((i < m + n) && (i < elementos + elementosInvalidos)) && encontrado == 0)
+
+            case 'a':
             {
-                if(vetor[i].status && vetor[i].linha == linha && vetor[i].coluna == coluna)
+                // Modo 'a'
+                sscanf(rawIn, "a [%d,%d] %d", &linha, &coluna, &valor);
+                i = 0;
+                p = triplas;
+                encontrado = 0;
+                while(((i < m + n) && (i < elementos + elementosInvalidos)) && encontrado == 0)
                 {
-                    // Elemento ativo, linhas e coluna iguais (encontrado)
-                    encontrado = 1;
-                    // printf("\tElemento [%d,%d] encontrado e ", linha, coluna);
-                    if(valor)
+                    if(p->status && p->linha == linha && p->coluna == coluna)
                     {
-                        // Alterar valor
-                        // printf("alterado\n");
-                        vetor[i].valor = valor;
+                        // Elemento ativo, linhas e coluna iguais (encontrado)
+                        encontrado = 1;
+                        // printf("\tElemento [%d,%d] encontrado e ", linha, coluna);
+                        if(valor)
+                        {
+                            // Alterar valor
+                            // printf("alterado\n");
+                            p->valor = valor;
+                        }
+                        else
+                        {
+                            // Remover valor
+                            // printf("removido / ");
+                            p->valor = 0;
+                            p->status = 0;
+                            elementos--;
+                            // printf("E = %d\n", elementos);
+                        }
+                    }
+                    else if(!p->status)// Elemento inativo
+                        elementosInvalidos++;
+                    p++;
+                    i++;
+                }
+
+                if(!encontrado && valor != 0)
+                {
+                    // Elemento novo
+                    if(elementos < m + n)
+                    {
+                        // Há espaço para adicionar
+                        p = triplas;
+                        for(i = 0; p->status == 1; i++)
+                        {
+                            p++;
+                        }
+                        p->linha = linha;
+                        p->coluna = coluna;
+                        p->valor = valor;
+                        p->status = 1;
+                        elementos++;
+                        // printf("\tElemento [%d,%d] NAO encontrado, adicionado em %d / ", linha, coluna, i);
+                        // printf("E = %d\n", elementos);
                     }
                     else
                     {
-                        // Remover valor
-                        // printf("removido / ");
-                        vetor[i].valor = 0;
-                        vetor[i].status = 0;
-                        elementos--;
-                        // printf("E = %d\n", elementos);
+                        // Não há espaço para adicionar
+                        printf("O VC esta' cheio.\n");
                     }
                 }
-                else if(!vetor[i].status)// Elemento inativo
-                    elementosInvalidos++;
-                i++;
+                
+                break;
             }
 
-            if(!encontrado && valor != 0)
+            case 'r':
             {
-                // Elemento novo
-                if(elementos < m + n)
+                // Modo 'r'
+                sscanf(rawIn, "r [%d,%d]", &linha, &coluna);
+                i = 0;
+                p = triplas;
+                encontrado = 0;
+                while(i <= elementos && encontrado == 0)
                 {
-                    // Há espaço para adicionar
-                    for(i = 0; vetor[i].status == 1; i++) {}
-                    vetor[i].linha = linha;
-                    vetor[i].coluna = coluna;
-                    vetor[i].valor = valor;
-                    vetor[i].status = 1;
-                    elementos++;
-                    // printf("\tElemento [%d,%d] NAO encontrado, adicionado em %d / ", linha, coluna, i);
-                    // printf("E = %d\n", elementos);
+                    if(p->status && p->linha == linha && p->coluna == coluna)
+                    {
+                        // Elemento ativo, linhas e coluna iguais (encontrado)
+                        encontrado = 1;
+                        // printf("Elemento encontrado!\n");
+                        printf("M[%d][%d] == %d\n", linha, coluna, p->valor);
+                    }
+                    p++;
+                    i++;
+                }
+
+                if(!encontrado)
+                {
+                    // Elemento nulo
+                    printf("M[%d][%d] == 0\n", linha, coluna);
+                }
+
+                break;
+            }
+
+            case 'p':
+            {
+                // Modo 'p'
+                if(elementos > 0)
+                {
+                    printf("VC: ");
+                    for(linha = 0; linha <= m; linha++)
+                    {
+                        for(coluna = 0; coluna <= n; coluna++)
+                        {
+                            i = 0;
+                            p = triplas;
+                            encontrado = 0;
+                            while(i < elementos && encontrado == 0)
+                            {
+                                if(p->status &&
+                                   p->linha == linha && p->coluna == coluna &&
+                                   p->valor)
+                                {
+                                    // Elemento ativo, linha e coluna certa
+                                    printf("(%d,%d,%d) ", p->linha,
+                                                          p->coluna,
+                                                          p->valor);
+                                    encontrado = 1;
+                                }
+                                p++;
+                                i++;
+                            }
+                        }
+                    }
+                    printf("\n");
                 }
                 else
                 {
-                    // Não há espaço para adicionar
-                    printf("O VC esta' cheio.\n");
+                    printf("O VC esta' vazio.\n");
                 }
-            }
-        }
-        else if(*rawIn == 'r')
-        {
-            // Modo 'r'
-            sscanf(rawIn, "r [%d,%d]", &linha, &coluna);
-            i = 0;
-            encontrado = 0;
-            while(i <= elementos && encontrado == 0)
-            {
-                if(vetor[i].status && vetor[i].linha == linha && vetor[i].coluna == coluna)
-                {
-                    // Elemento ativo, linhas e coluna iguais (encontrado)
-                    encontrado = 1;
-                    // printf("Elemento encontrado!\n");
-                    printf("M[%d][%d] == %d\n", linha, coluna, vetor[i].valor);
-                }
-                i++;
-            }
 
-            if(!encontrado)
-            {
-                // Elemento nulo
-                printf("M[%d][%d] == 0\n", linha, coluna);
-            }
-        }
-        else if(*rawIn == 'p')
-        {
-            // Modo 'p'
-            if(elementos > 0)
-            {
-                printf("VC: ");
-                for(linha = 0; linha <= m; linha++)
-                {
-                    for(coluna = 0; coluna <= n; coluna++)
-                    {
-                        i = 0;
-                        encontrado = 0;
-                        while(i < elementos && encontrado == 0)
-                        {
-                            if(vetor[i].status &&
-                               vetor[i].linha == linha && vetor[i].coluna == coluna &&
-                               vetor[i].valor)
-                            {
-                                // Elemento ativo, linha e coluna certa
-                                printf("(%d,%d,%d) ", vetor[i].linha,
-                                                      vetor[i].coluna,
-                                                      vetor[i].valor);
-                                encontrado = 1;
-                            }
-                            i++;
-                        }
-                    }
-                }
-                printf("\n");
-            }
-            else
-            {
-                printf("O VC esta' vazio.\n");
+                break;
             }
         }
     }
+
     // while(*(fgets(rawIn, INMAX, stdin)) != 't')
     // {
     //     i = 0;
@@ -179,5 +213,6 @@ int main()
 
     // printf("E = %d\n", elementos);
 
+    free(triplas);
     return 0;
 }
