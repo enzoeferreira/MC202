7cd325e482fc97e7f665b0d7cf4fb025044a72aa
enzoeferreira <enzoeferreira@gmail.com>
2023-05-15 19:37:45 -0300

    [ADDED] main.c

---
+++ Labs/lab23/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * Header
+ */
+struct node {
+    int data;
+    node *prev, *next;
+};
+typedef struct node node;
+
+struct list {
+    node *head, *tail;
+};
+typedef struct list list;
+
+/**
+ * Library
+ */
+
+/**
+ * Main
+ */
+int main() {
+
+}
