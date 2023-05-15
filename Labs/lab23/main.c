ab3cc1a006759726a875daf357381714bc41a9a0
enzoeferreira <enzoeferreira@gmail.com>
2023-05-15 20:20:24 -0300

    [ADDED] startList(), insertNode(), print() e extração de lista

--- Labs/lab23/main.c
+++ Labs/lab23/main.c
@@ -5,7 +5,7 @@
  */
 struct node {
     int data;
-    node *prev, *next;
+    struct node *prev, *next;
 };
 typedef struct node node;
 
@@ -19,8 +19,91 @@ typedef struct list list;
  */
 
 /**
+ * Cria uma lista com dummys no inicio e fim
+ * 
+ * @return 1) NULL, caso ocorra falha ao alocar a lista ou dummys
+ * @return 2) L, onde L é um apontador para a lista
+ */
+list* startList() {
+    list *L = malloc(sizeof(list)); // Tenta alocar lista
+    if(!L)
+        return NULL;
+    
+    node *head = malloc(sizeof(node)); // Tenta alocar dummy pra head
+    if(!head)
+        return NULL;
+    L->head = head;
+    
+    node *tail = malloc(sizeof(node)); // Tenta alocar dummy pra tail
+    if(!tail)
+        return NULL;
+    L->tail = tail;
+
+    L->head->prev = NULL;
+    L->head->next = L->tail;
+    L->tail->prev = L->head;
+    L->tail->next = NULL;
+
+    return L;
+}
+
+/**
+ * Adiciona um nó com o dado no fim da lista
+ * 
+ * @param L lista
+ * @param data dado a ser adicionado
+ * 
+ * @return 1) 0, em caso de erro ao alocar espaço para nó
+ * @return 2) 1, em caso de sucesso
+ */
+int insertNode(list* L, int data) {
+    node *n = malloc(sizeof(node)); // Tenta alocar nó
+    if(!n)
+        return 0;
+
+    n->data = data;
+    n->next = L->tail;
+    n->prev = L->tail->prev;
+    L->tail->prev->next = n;
+    L->tail->prev = n;
+    return 1;
+}
+
+/**
+ * Printa a lista de head a tail
+ * 
+ * @param L lista
+ */
+void print(list* L) {
+    printf("A: {");
+
+    node *p = L->head->next; // Aponta para o primeiro elemento
+    while(p != L->tail->prev) {
+        printf(" %d,", p->data);
+        p = p->next;
+    }
+    printf(" %d }", p->data); // Último elemento
+}
+
+/**
  * Main
  */
 int main() {
-
+    list *L = startList();
+    
+    /**
+     * Extração de lista pela entrada
+     */
+    char c;
+    int num = 0;
+    while((c = getchar()) != '}') { // Lista não acabou
+        if(c >= '0' && c <= '9') // Caractére é um digito
+            num = num * 10 + c - '0'; // Conversão char -> int
+        else if(c == ',') {
+            insertNode(L, num);
+            num = 0;
+        }
+    }
+    insertNode(L, num); // Insere o último digito
+    print(L);
 }
