076b301ad5e9d455ccf7a37d9c2df6482a8b1485
enzoeferreira <enzoeferreira@gmail.com>
2023-06-13 17:14:09 -0300

    [ADDED] h1 e h2 para hashing

--- Labs/lab26/hashing.c
+++ Labs/lab26/hashing.c
@@ -10,6 +10,28 @@ struct tuple {
 };
 typedef struct tuple tuple;
 
+unsigned long sdbm(unsigned char *str) {
+    unsigned int size = len(str) / sizeof(char);
+    unsigned int hash = 0;
+	unsigned int i = 0;
+
+	for (i = 0; i < size; str++, i++)
+		hash = (*str) + (hash << 6) + (hash << 16) - hash;
+
+	return hash;
+}
+
+unsigned long djb2(unsigned char *str) {
+    unsigned long hash = 5381;
+    int c;
+
+    while ((c = *str++))
+    hash = ((hash << 5) + hash) ^ c; // hash * 33 XOR c
+
+    return hash;
+}
+
+
 int main() {
 
 }
