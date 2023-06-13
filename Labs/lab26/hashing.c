b6b025f2902a62bdd9647869b2452e63cb3715d2
enzoeferreira <enzoeferreira@gmail.com>
2023-06-13 17:09:26 -0300

    [ADDED] struct tuple

--- Labs/lab26/hashing.c
+++ Labs/lab26/hashing.c
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAXSTRING 101
+
+struct tuple {
+    unsigned char string[MAXSTRING];
+    unsigned long timestamp;
+};
+typedef struct tuple tuple;
+
 int main() {
 
 }
