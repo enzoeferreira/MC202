f3cb6c45a217999ee6b678057073e0e8d5462249
enzoeferreira <enzoeferreira@gmail.com>
2023-06-13 18:44:54 -0300

    [ADDED] inserção

--- Labs/lab26/hashing.c
+++ Labs/lab26/hashing.c
@@ -39,8 +39,19 @@ unsigned long djb2(unsigned char* str) {
     return hash;
 }
 
-unsigned long hashing(hashTable* T, unsigned char* str, unsigned long* count) {
-    return (djb2(str) + (*count) * sdbm(str)) % T->maxSize;
+/**
+ * Utiliza sondagem com incremento por hashing dupla para calcular o hash de uma cadeia
+ * h1: djb2
+ * h2: sdbm
+ * 
+ * @param T tabela hash
+ * @param str cadeia que entrará na função de hashing
+ * @param count contagem de colisões
+ * 
+ * @return hash da cadeia
+ */
+unsigned long hashing(hashTable* T, unsigned char* str, unsigned long count) {
+    return ((djb2(str) + (count * sdbm(str))) % T->maxSize);
 }
 
 /**
@@ -66,6 +77,45 @@ hashTable* createTable(unsigned long maxSize) {
     return T;
 }
 
+/**
+ * Insere uma cadeia na tabela hash
+ * 
+ * @param T tabela hash
+ * @param str cadeia a ser adicionada
+ * 
+ * @return 1) 0, caso cadeia já esteja na tabela
+ * @return 2) 1, caso cadeia seja inserida na tabela
+ */
+short int insert(hashTable* T, unsigned char* str) {
+    unsigned long count = 0;
+    unsigned long hash = hashing(T, str, count);
+    while(T->array[hash].key != -1) {
+        if(!strcmp(T->array[hash].string, str)) // String encontrada
+            return 0;
+        hash = hashing(T, str, ++count);
+    }
+    T->array[hash].key = 0;
+    strcpy(T->array[hash].string, str);
+    T->array[hash].timestamp = T->timestamp++;
+    return 1;
+}
+
+/**
+ * Printa a tabela hash no formato:
+ * 1) ['pos'] vazio
+ * 2) ['pos'] 'string' ('key'): t = 'timestamp'
+ * 
+ * @param T tabela hash
+ */
+void print(hashTable* T) {
+    for(int i = 0; i < T->maxSize; i++) {
+        if(T->array[i].key == -1)
+            printf("[%d] vazio\n", i);
+        else
+            printf("[%d] %s (%d): t = %d\n", i, T->array[i].string, T->array[i].key, T->array[i].timestamp);
+    }
+}
+
 int main() {
     unsigned short existingTable = 0;
     unsigned long maxSize, count;
@@ -77,38 +127,48 @@ int main() {
         getchar(); // Retira espaço
 
         switch(cmd) {
-            case 'c':
-            printf("CRIANDO tabela hash\n");
-            if(existingTable) {
-                free(T->array);
-                free(T);
+            case 'c': { // Criação de tabela hash
+                if(existingTable) {
+                    free(T->array);
+                    free(T);
+                }
+                scanf("%lu", &maxSize);
+                getchar();
+                T = createTable(maxSize);
+                for(int i = 0; i < maxSize; i++)
+                    T->array[i].key = -1; // Marca posições como vazias
             }
-            scanf("%lu", &maxSize);
-            T = createTable(maxSize);
-            for(int i = 0; i < maxSize; i++)
-                T->array[i].key = -1; // Marca posições como vazias
-            for(int i = 0; i < maxSize; i++)
-                printf("posicao %d: %d\n", i, T->array[i].key);
-
             break;
 
-            case 'i':
-            printf("INSERINDO cadeia na tabela hash\n");
-            scanf("%[^\n]", string);
-            count = 0;
-            printf("%d", hashing(T, string, &count));
+            case 'i': { // Inserção de cadeia na tabela hash
+                scanf("%[^\n]", string);
+                getchar();
+                short int unique = insert(T, string);
+                if(!unique)
+                    printf("\tELEMENTO REPETIDO.\n");
+            }
             break;
 
-            case 'r':
-            printf("REMOVENDO cadeia da tabela hash\n");
+            case 'r': { // Remove cadeia da tabela hash
+
+            }
             break;
 
-            case 'b':
-            printf("IMPRIMINDO TIMESTAMP de cadeia na tabela hash\n");
+            case 'b': { // Imprime a timestamp da cadeia
+
+            }
+            break;
+
+            case 'p': { // Print da tabela hash para debug
+                print(T);
+            }
             break;
         }
 
-        free(T);
-        cmd = getchar();
+        cmd = getchar(); // Novo comando
     }
+
+    // Libera memória alocada
+    free(T->array);
+    free(T);
 }
