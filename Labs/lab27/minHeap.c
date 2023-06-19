bed6c540c46acadd4fd23f96a49620e111dee7b6
enzoeferreira <enzoeferreira@gmail.com>
2023-06-19 18:24:52 -0300

    [ADDED] structs node e heap

--- Labs/lab27/minHeap.c
+++ Labs/lab27/minHeap.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct node {
+    long key, priority;
+};
+typedef struct node node;
+
+struct heap {
+    long size, maxSize;
+    node* array;
+};
+typedef struct heap heap;
+
 int main() {
 
 }
