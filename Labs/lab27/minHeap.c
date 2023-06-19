66414daff85d9caf91d47074d64066c98e050caa
enzoeferreira <enzoeferreira@gmail.com>
2023-06-19 18:37:40 -0300

    [ADDED] createHeap() e killHeap()

--- Labs/lab27/minHeap.c
+++ Labs/lab27/minHeap.c
@@ -12,7 +12,42 @@ struct heap {
 };
 typedef struct heap heap;
 
+/**
+ * Cria um heap e um array com espaço para 'maxSize' nós
+ * 
+ * @param maxSize Tamanho do heap
+ * 
+ * @return 1) NULL, caso falhe em alocar espaço para Heap/Array
+ * @return 2) H, ponteiro para Heap
+ */
+heap* createHeap(long maxSize) {
+    heap *H = malloc(sizeof(heap));
+    if(!H)
+        return NULL;
+    node *array = malloc(maxSize * sizeof(node));
+    if(!array)
+        return NULL;
+    
+    H->array = array;
+    H->size = 0;
+    H->maxSize = maxSize;
+
+    return H;
+}
+
+/**
+ * Libera a memória alocada para um heap
+ * 
+ * @param H Heap a ser liberado
+ */
+void killHeap(heap* H) {
+    free(H->array);
+    free(H);
+    return;
+}
+
 int main() {
+    heap *H;
     short existingHeap = 0;
     long inMaxSize, inKey, inPriority;
     char cmd;
@@ -22,6 +57,10 @@ int main() {
         switch(cmd) {
             case 'c': {
                 scanf(" %li", &inMaxSize);
+                if(existingHeap)
+                    killHeap(H);
+                H = createHeap(inMaxSize);
+                existingHeap = 1;
             }
             break;
 
