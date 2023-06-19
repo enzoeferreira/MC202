fd66b8c21967e8e80d0e2588b374b369cf4e10e2
enzoeferreira <enzoeferreira@gmail.com>
2023-06-19 18:30:59 -0300

    [ADDED] modos

--- Labs/lab27/minHeap.c
+++ Labs/lab27/minHeap.c
@@ -3,16 +3,43 @@
 
 struct node {
     long key, priority;
-    node *left, *right, *parent;
 };
 typedef struct node node;
 
 struct heap {
     long size, maxSize;
-    node* array;
+    node *array;
 };
 typedef struct heap heap;
 
 int main() {
+    short existingHeap = 0;
+    long inMaxSize, inKey, inPriority;
+    char cmd;
 
+    cmd = getchar();
+    while(cmd != 't') {
+        switch(cmd) {
+            case 'c': {
+                scanf(" %li", &inMaxSize);
+            }
+            break;
+
+            case 'i': {
+                scanf(" %li %li", &inKey, &inPriority);
+            }
+            break;
+
+            case 'r': {
+
+            }
+            break;
+
+            case 'm': {
+
+            }
+            break;
+        }
+        cmd = getchar(); // Recebe próximo comando
+    }
 }
